7d0ce7b03c7266643182c65cbea855692c01ec7c
Christopher Hill <chill@mirabeau.nl>
2019-07-25 11:21:30 +0200

    Added color

--- Easy-Login/Easy-Azure-login.ps1
+++ Easy-Login/Easy-Azure-login.ps1
@@ -3,10 +3,10 @@ Connect-AzAccount | out-null
 
 $Subscription = Get-AzSubscription
 Clear-Host
-Write-Host "Here are your following subscriptions"
-
+Write-Host -ForegroundColor Yellow "Here are your following subscriptions:"
+ 
 For ($i=0; $i -lt $Subscription.Length; $i++) {
     Write-Host $i $Subscription.name[$i]
 }
-$i = Read-Host -Prompt "Please select your Subscription" 
+$i = Read-Host -Prompt "Please select the subscription your require" 
 Select-AzSubscription -SubscriptionName $Subscription.name[$i]
